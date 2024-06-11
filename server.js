d683fe12f832a227c10f41a0f11561710c8f1598
Swati Singh <pariswati1103@gmail.com>
2024-06-11 13:19:32 +0530

    updating server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 //let's tackle cors
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://prayas75-2-0-client.onrender.com",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
